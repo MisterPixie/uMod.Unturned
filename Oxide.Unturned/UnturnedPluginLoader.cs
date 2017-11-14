a151dcdfd3c60699a2fca60338d960f99f2e59e2
Luke Spragg <luke@spragg.me>
2017-11-14 18:40:38 -0500

    Initial commit of split/migrated base

---
+++ Oxide.Unturned/UnturnedPluginLoader.cs
@@ -0,0 +1,13 @@
+﻿using Oxide.Core.Plugins;
+using System;
+
+namespace Oxide.Game.Unturned
+{
+    /// <summary>
+    /// Responsible for loading core plugins for "Unturned"
+    /// </summary>
+    public class UnturnedPluginLoader : PluginLoader
+    {
+        public override Type[] CorePlugins => new[] { typeof(UnturnedCore) };
+    }
+}
