c07c4942bbd6021c22ac029784a88d0608b0c33d
Luke Spragg <luke@spragg.me>
2018-11-28 12:40:03 -0500

    Fixed silly typo that was overlooked in .bat file

--- resources/_start-example.bat
+++ resources/_start-example.bat
@@ -3,7 +3,7 @@ cls
 :start
 echo Starting server...
 
-Unturned.exe -batchmode -nographics -bind 0.0.0.0 -port 27015 -maxplayers 10 -map PEI -name "My uMod Server" -welcome "Weclome to my uMod server!" +secureserver "uMod"
+Unturned.exe -batchmode -nographics -bind 0.0.0.0 -port 27015 -maxplayers 10 -map PEI -name "My uMod Server" -welcome "Welcome to my uMod server!" +secureserver "uMod"
 
 echo.
 echo Restarting server...
